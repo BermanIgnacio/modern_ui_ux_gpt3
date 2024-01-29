146ae077091c30ebb950f4d08fd3bcb8f8ee549f
Berman Ignacio <bermanignacio94@hotmail.com>
2024-01-29 15:19:38 -0300

    added Bran, Feature Components & WhatGPT3 Container

--- src/Components/Brand/Brand.jsx
+++ src/Components/Brand/Brand.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./Brand.css"
+import { google, atlassian, dropbox, shopify, slack } from "./imports"
+
 
 function Brand(){
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="Logo Google" />
+      </div>
+      <div>
+        <img src={slack} alt="Logo Slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Logo aAlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Logo Dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="Logo Shopify" />
+      </div>
     </div>
   )
 }
---
+++ src/Components/Brand/imports.js
@@ -0,0 +1,13 @@
+import google from "../../assests/google.png"
+import dropbox from "../../assests/dropbox.png"
+import atlassian from "../../assests/atlassian.png"
+import slack from "../../assests/slack.png"
+import shopify from "../../assests/shopify.png"
+
+export { 
+  google,
+  dropbox,
+  atlassian,
+  slack,
+  shopify
+}
