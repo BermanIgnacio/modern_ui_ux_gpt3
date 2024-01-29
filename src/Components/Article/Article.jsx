e261f1c873fb53d51be9673def1ad10e400ff73f
Berman Ignacio <bermanignacio94@hotmail.com>
2024-01-29 19:21:08 -0300

    added Article Component & Footer,Blog Container

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Article.css"
 
-function Article(){
+function Article({imgURL,title,date}){
   return (
-    <div>
-      Article
+    <div className='gpt3__article'>
+      <div className='gpt3__article-img'>
+        <img src={imgURL} alt="blog img" />
+      </div>
+      <div className='gpt3__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
