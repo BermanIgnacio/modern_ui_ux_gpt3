8ccfed3c6f330e19361584bc872a94c1f2aa184b
Berman Ignacio <bermanignacio94@hotmail.com>
2024-01-29 14:07:53 -0300

    added Navbar & Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./App.css"
-
+import { Brand, CTA, Navbar } from "./Components";
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./Containers";
 
 function App() {
    return(
       <div className='App'>
-        App
+         <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+         </div>
+         <Brand />
+         <WhatGPT3 />
+         <Features />
+         <Possibility />
+         <CTA />
+         <Blog />
+         <Footer />
       </div>
    )
 }
