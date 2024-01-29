6153c69dbacda4736a1b52fcfe2830a50f78ddf6
Berman Ignacio <bermanignacio94@hotmail.com>
2024-01-29 17:13:26 -0300

    added CTA Component & Features,Possibility Container

--- src/Containers/Possibility/Possibility.jsx
+++ src/Containers/Possibility/Possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Possibility.css"
+import possibillityImage from "../../assests/possibillityImage.png"
 
 function Possibility(){
   return (
-    <div>
-      Possibility
+    <div className='gtp3__possibillity section__padding'>
+      <div className='gtp3__possibillity-img'>
+        <img src={possibillityImage} alt="possibillity" />
+      </div>
+      <div className='gtp3__possibillity-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h5>Request Early Access to Get Started</h5>
+      </div>
     </div>
   )
 }
